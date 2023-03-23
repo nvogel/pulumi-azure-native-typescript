6bb9703503eba678de0fde77e228cdc3b9cd7567
Nicolas Vogel <238910+nvogel@users.noreply.github.com>
2023-03-23 15:26:25 +0400

    Update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pulumi-azure-native-typescript
-Pulumi template
+
+# Pulumi template for typescript with Azure-native
+
+## Usage
+
+```bash
+mkdir demo
+pulumi new https://github.com/nvogel/pulumi-azure-native-typescript --dir demo
+```
--- src/common.ts
+++ src/common.ts
@@ -1,23 +1,22 @@
 import * as resources from '@pulumi/azure-native/resources'
-import * as pulumi from '@pulumi/pulumi'
+import { getStack, getProject, Config } from '@pulumi/pulumi'
 
-export const stack = pulumi.getStack()
-export const projectName = pulumi.getProject()
+export const projectName = getProject()
+export const stack = getStack()
 
-export const projectConfig = new pulumi.Config('project')
-export const projectUrl = projectConfig.require('url')
+const azureNativeConfig = new Config('azure-native')
+export const subscriptionIdConfig = azureNativeConfig.require('subscriptionId')
+
+const projectConfig = new Config('project')
 
 export const tags = {
-    projectName,
-    stack,
-    projectUrl,
+    'project:name': projectName,
+    'project:url': projectConfig.require('url'),
+    'pulumi:stack': stack,
 }
 
 const resourceGroupName = `rg-${projectName}-${stack}`
 export const resourceGroup = new resources.ResourceGroup(resourceGroupName, {
     resourceGroupName: resourceGroupName,
-    tags: tags,
+    tags,
 })
-
-export const azureNativeConfig = new pulumi.Config('azure-native')
-export const subscriptionIdConfig = azureNativeConfig.require('subscriptionId')
